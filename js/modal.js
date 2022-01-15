9c7dc2e62762706f0aca6437e4891b7be79a6a2d
OlhaVoloshenko <voloshenko.olya@gmail.com>
2022-01-15 12:06:22 +0200

    Вирівнюємо блок Mein-hero
    Вирівнюємо блок Mein-hero для усіх варіантів

--- js/modal.js
+++ js/modal.js
@@ -11,7 +11,7 @@
 	refs.backdrop.addEventListener('click', logBackDropClick);
   
     function toggleModal() {
-      document.body.classList.toggle("modal-open");
+      document.body.classList.toggle("open-modal");
       refs.backdrop.classList.toggle('backdrop__is-hidden');
     }
 	
